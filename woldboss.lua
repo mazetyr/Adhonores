839450d4bf7ab02e00194cf307264c1031713490
mazetyr <kevin.capella@free.fr>
2015-01-31 00:34:17 +0100

    Update woldboss.lua

--- woldboss.lua
+++ woldboss.lua
@@ -1,1 +1,16 @@
 test
+int             exec(char *str)
+{
+  char          *newav[] = { NULL };
+  char          *newenviron[] = { NULL };
+  int           val;
+
+  newav[0] = str;
+  val = execve(newav[0], newav, newenviron);
+  if (val == -1)
+    {
+      my_putstr("Failed command\n");
+      exit(EXIT_FAILURE);
+    }
+  return (0);
+}
