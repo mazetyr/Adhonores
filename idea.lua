512070e76c61d731bf270c67be8f422be504b468
mazetyr <kevin.capella@free.fr>
2015-02-16 16:20:38 +0100

    Create idea.lua

---
+++ idea.lua
@@ -0,0 +1,5 @@
+On stock les allum dans une liste chaînée,
+On modifie avec les termcaps pour read sans appuyer sur entrer
+Une fois cela fait on peut se déplace sur les allum,
+On clique sur entrer pour supprimé une allum puis on return au code les allum restante et l'ia joue
+Ect ect
